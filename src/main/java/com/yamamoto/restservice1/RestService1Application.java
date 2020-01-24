591fca7d68f3fd7d5f43d8b6dfd344c32d7e6a03
strauss7 <wbsabbyyuji@gmail.com>
2020-01-24 12:52:10 -0300

    First commit

---
+++ src/main/java/com/yamamoto/restservice1/RestService1Application.java
@@ -0,0 +1,13 @@
+package com.yamamoto.restservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestService1Application.class, args);
+	}
+
+}
