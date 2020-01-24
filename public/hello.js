5e537ab85ec23794cb53125b959b3934decc5dd5
strauss7 <wbsabbyyuji@gmail.com>
2020-01-24 13:40:06 -0300

    add httpClient dependency

---
+++ public/hello.js
@@ -0,0 +1,8 @@
+angular.module('demo', []).controller(
+		'Hello',
+		function($scope, $http) {
+			$http.get('http://rest-service.guides.spring.io/greeting').then(
+					function(response) {
+						$scope.greeting = response.data;
+					});
+		});
